ac8de06efd8fd668d1f16d9d1b900b8dfd2b1973
kw1c-KarimovaNargiz <giesje002@gmail.com>
2025-02-23 21:21:06 +0100

    styling bg LoginPage

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -33,38 +33,38 @@ export default function LoginPage() {
   }, []);
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-purple-700 to-blue-500 relative">
-      <div className="bg-white p-8 rounded-xl shadow-lg w-96">
-        <h2 className="text-2xl font-bold text-center mb-4">Welkom, log in</h2>
+    <div className="flex items-center justify-center min-h-screen bg-blue-950 relative">
+      <div className="bg-white p-6 rounded-lg shadow-md w-96">
+        <h2 className="text-xl font-semibold text-center mb-4">Welkom, log in</h2>
         <form onSubmit={checkLogin}>
-          <div className="mb-4">
-            <label className="block text-gray-700">Gebruikersnaam</label>
+          <div className="mb-3">
+            <label className="block text-gray-600">Gebruikersnaam</label>
             <input
               type="text"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
-              className="w-full p-2 border border-gray-300 rounded-lg focus:ring focus:ring-indigo-300"
+              className="w-full p-2 border border-gray-300 rounded-md focus:ring focus:ring-blue-300"
               required
             />
           </div>
-          <div className="mb-4">
-            <label className="block text-gray-700">Wachtwoord</label>
+          <div className="mb-3">
+            <label className="block text-gray-600">Wachtwoord</label>
             <input
               type="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className="w-full p-2 border border-gray-300 rounded-lg focus:ring focus:ring-indigo-300"
+              className="w-full p-2 border border-gray-300 rounded-md focus:ring focus:ring-blue-300"
               required
             />
           </div>
           <button
             type="submit"
-            className="w-full bg-purple-700 text-white p-2 rounded-lg hover:bg-blue-500 transition"
+            className="w-full bg-blue-800 text-white p-2 rounded-md hover:bg-blue-700 transition"
           >
             Inloggen
           </button>
         </form>
-        {error && <p className="text-red-500 text-sm mt-3">{error}</p>}
+        {error && <p className="text-red-500 text-sm mt-2">{error}</p>}
       </div>
 
       <Timer showTips={true} />
